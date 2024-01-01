e8ac179217f891164b60146598820ad421f3f421
Lajos Tornyi <ltornyi@gmail.com>
2024-01-01 17:57:37 +0100

    recent

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import './globals.css'
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
     </html>
-  )
+  );
 }
